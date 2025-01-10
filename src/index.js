00010ca0c458413ed8488a5cf707dd09e7490246
Mamoudou Barry <barrymamoudou982@gmail.com>
2025-01-10 16:10:16 -0500

    completed pair lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const createTaskFormElement = document.getElementById('create-task-form');
+createTaskFormElement.addEventListener('submit',  (event) => {
+event.preventDefault()
+
+ const newTaskDescriptionInputElement= document.getElementById('new-task-description')
+
+ const liElement = document.createElement('li')
+ liElement.textcontent = newTaskDesccriptionInput.value
+
+  
+  const delBtn = document.createElement('button');
+  deleteButton.textcontent = "X"
+  deletebutton.setAttribute('data-description' , newTaskDescriptionInputElement.value)
+  deletteButton.addEventListener('click', (event) => {
+    lieElement.remove()
+    event.target.parentNode.remove()
+    })
